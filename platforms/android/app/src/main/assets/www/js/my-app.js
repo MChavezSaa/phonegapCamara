e812769173b4ad702a25c26d3cb2e668b829ec4b
Matias Chavez <matroxmega@gmail.com>
2019-05-10 10:44:13 -0400

    initial commit

---
+++ platforms/android/app/src/main/assets/www/js/my-app.js
@@ -0,0 +1,38 @@
+// Initialize app
+var myApp = new Framework7();
+  
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+var app = new Framework7({
+    // App root element
+    root: '#app',
+    // App Name
+    name: 'My App',
+    // App id
+    id: 'com.myapp.test',
+    // Enable swipe panel
+    panel: {
+      swipe: 'left',
+    },
+    // Add default routes
+    routes: [
+      {
+        path: '/about/',
+        url: 'about.html',
+      },
+    ]
+    // ... other parameters
+  });
+
+var mainView = app.views.create('.view-main');
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+    console.log("Device is ready!");
+    $$("#btn-tomar").on("click",tomarFoto);
+});
+
+function tomarFoto(){
+  console.log("click en btn tomar foto");
+}
